a63555ac4b8025a9a9536eab30995f30a71e1442
diegosorio76 <118573914+diegosorio76@users.noreply.github.com>
2022-11-22 21:28:45 -0500

    Se sube de nuevo la pantalla con cambios

---
+++ src/Componentes/Laboratorio.js
@@ -0,0 +1,93 @@
+import React from 'react'
+import logo_clinica from '../Imagenes/logo_clinica.png'
+import nombre_clinica from '../Imagenes/nombre_clinica.png'
+import '../Laboratorio.css'
+import { Inicio } from './Inicio'
+
+
+export const Laboratorio = () => {
+  return (
+    <body>
+      <header className='usuario'>
+        <text>Bienvenido(a) DIEGO ALEJANDRO OSORIO HERNANDEZ</text>
+      </header>
+      <header>
+        <div className='encabezado'>
+            <img src={logo_clinica} alt='logo clinica' width="60px"/>
+            <img src={nombre_clinica} alt='nombre clinica' width="140px" height="55px" />
+        </div>
+        <div className='botones'>
+          <div><text className='oficina'>Oficina Virtual</text></div>
+          <text className='oficina1'>Linea Bogotá 4700000 Nacional 01800789456</text>
+        </div>
+      </header>
+      <nav>
+        <ul className='navegacion'>
+          <li><a href={Inicio}>Inicio</a></li>
+          <li><a href={Inicio}>Red de Atención</a></li>
+          <li><a href={Inicio}>Afiliados</a></li>
+          <li><a href={Inicio}>Contactanos</a></li>
+        </ul>   
+      </nav>
+      <div className='servicios'>
+        <text>Inicio /  Servicios en atención de salud  / Laboratorio</text>
+      </div>
+      <div className='titulo'>
+        <text>Laboratorio</text>
+      </div>
+      <div className='afiliado'>
+        <text>DIEGO ALEJANDRO OSORIO HERNANDEZ - CC1003456789</text>
+      </div>
+
+      <div className='seleccion'>
+        <text>Seleccione el año y mes de la toma del laboratorio que deseas consultar</text>
+      </div>
+      <div className='selectorfecha'>
+        <center>
+        <input type="month" name="fecha" id="fecha" class="form-control"/>
+        <button>         
+            BUSCAR 
+          </button>
+        </center>
+      </div>
+      <center>
+        <div className='cuadro2'>
+          <h4>LABORATORIO CLINICO</h4>
+            <div className='textocuadro'>
+              <text>Numero: 456</text> <br></br> 
+              <text></text> <br></br>         
+              <text>Fecha:15/11/2022</text>
+            </div>
+          
+          <button className="Descargar">         
+             DESCARGAR
+          </button>
+          <button className="Enviar">         
+             ENVIAR
+          </button>
+        
+        </div>
+      </center>
+      <footer>
+        <div className='fin'>
+          <div>
+            <img src={logo_clinica} alt='logo clinica' width="60px"/>
+            <img src={nombre_clinica} alt='nombre clinica' width="140px" height="55px" />
+          </div>
+          <div><text className='oficina2'>Oficina Virtual</text></div>
+          <div>
+            Preguntas Frecuentes
+            <br></br>Contáctanos<br></br>
+            Clinica Imperial Integral<br></br>
+            Bogotá     4700000<br></br>
+            Nacional  018000789456
+          </div>
+        </div>
+        <div className='pie'>
+          <div>@Copyright 2022 Clinica Imperial  Integral - Derechos reservados.</div>
+          <div>Cualquier uso de este sitio constituye su aceptación y conformidad de los Términos y condiciones y Politica de privacidad.</div>
+        </div>
+      </footer>
+    </body>
+  )
+}
